0cd57d5c51c658fe8ce32f0b048417b4a3c7bfad
Yury <yurha@protonmail.com>
2023-04-04 16:23:49 +0300

    ready to pull

--- orbitar.user.js
+++ orbitar.user.js
@@ -131,8 +131,8 @@
             }
 
             const signaturreLinks = signature.querySelectorAll('a[href*="/p"]');
-            const signatureSiteLink = signature.querySelector('a[href*="/s"]');
-            const signatureAuthorLink = signature.querySelector('a.i-user');
+            const signatureSiteLink = signature.querySelector('a[href*="/s/"]');
+            const signatureAuthorLink = signature.querySelector('a[href*="/u/"]');
             let postSite = null;
             if (signatureSiteLink && signatureSiteLink.innerHTML) {
                 postSite = signatureSiteLink.innerHTML;
@@ -154,29 +154,33 @@
             }
 
             const postAuthor = signature.querySelector('a.i-user').innerText;
-            if (window.location.href.indexOf("/s/") < 0) {
-                if (postId) {
-                    const hidePostEl = document.createElement('span');
-                    hidePostEl.className = 'BO__hide-post';
-                    hidePostEl.innerHTML = '×';
-                    hidePostEl.dataset.postId = postId;
-                    hidePostEl.dataset.postAuthor = postAuthor;
-                    signature.insertAdjacentElement('beforeend', hidePostEl);
-                }
+
+            if (postId) {
+                const hidePostEl = document.createElement('span');
+                hidePostEl.className = 'BO__hide-post';
+                hidePostEl.innerHTML = '×';
+                hidePostEl.title = "скрыть пост";
+                hidePostEl.dataset.postId = postId;
+                hidePostEl.dataset.postAuthor = postAuthor;
+                signature.insertAdjacentElement('beforeend', hidePostEl);
             }
 
-            if (postSite) {
-                const hideSiteEl = document.createElement('span');
-                hideSiteEl.className = 'BO__hide-site';
-                hideSiteEl.innerHTML = '×';
-                hideSiteEl.dataset.postSite = postSite;
-                signatureSiteLink.insertAdjacentElement('afterend', hideSiteEl);
+            if (window.location.href.indexOf("/s/") < 0) {
+                if (postSite) {
+                    const hideSiteEl = document.createElement('span');
+                    hideSiteEl.className = 'BO__hide-site';
+                    hideSiteEl.innerHTML = '×';
+                    hideSiteEl.title = "игнорировать " + postSite;
+                    hideSiteEl.dataset.postSite = postSite;
+                    signatureSiteLink.insertAdjacentElement('afterend', hideSiteEl);
+                }
             }
 
             if (postAuthor) {
                 const hideAuthorEl = document.createElement('span');
                 hideAuthorEl.className = 'BO__hide-username';
                 hideAuthorEl.innerHTML = '×';
+                hideAuthorEl.title = "игнорировать " + postAuthor;
                 hideAuthorEl.dataset.postAuthor = postAuthor;
                 signatureAuthorLink.insertAdjacentElement('afterend', hideAuthorEl);
             }
@@ -207,6 +211,7 @@
                 const hideCAuthorEl = document.createElement('span');
                 hideCAuthorEl.className = 'BO__hide-username';
                 hideCAuthorEl.innerHTML = '×';
+                hideCAuthorEl.title = "игнорировать " + commentAuthor;
                 hideCAuthorEl.dataset.postAuthor = commentAuthor;
                 signatureCAuthorLink.insertAdjacentElement('afterend', hideCAuthorEl);
             }
@@ -332,6 +337,7 @@
     const config = { attributes: false, childList: true, subtree: true };
     let newComments = 0;
     let previousUrl = '';
+    let lastUrl = '';
     let count = 0;
     const callback = function () {
         doStuff();
@@ -892,6 +898,11 @@
         `;
 
     const newCommentsNavCss = `
+        .commentBody{
+            padding: 7px;
+            scroll-margin-top: 80px;
+            border: 1px solid transparent;
+        }
         .prevC {
         width: 40px;
         height: 40px;
@@ -1047,8 +1058,7 @@
             newComments[count + 1].childNodes[0].style.border = "none";
         }
         element.childNodes[0].style.border = "1px solid Gray";
-        element.childNodes[0].style.padding = "7px";
-        element.childNodes[0].scrollIntoView({ behavior: 'smooth', block: 'center' });
+        element.childNodes[0].scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
 
     function onNext(event) {
@@ -1064,17 +1074,17 @@
         var element = newComments[count];
         newComments[count - 1].childNodes[0].style.border = "none";
         element.childNodes[0].style.border = "1px solid Gray";
-        element.childNodes[0].style.padding = "7px";
-        element.childNodes[0].scrollIntoView({ behavior: 'smooth', block: 'center' });
+        element.childNodes[0].scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
 
     function doCommentNav() {
-        count = 0;
+        if (location.href !== lastUrl) {
+            count = 0;
+            lastUrl = location.href;
+        }
 
         if (newComments.length > 0) {
             newComments[0].childNodes[0].style.border = "1px solid Gray";
-            newComments[0].childNodes[0].style.padding = "7px";
-            newComments[0].childNodes[0].scrollIntoView({ behavior: 'smooth', block: 'center' });
         }
         if (newComments.length > 1 && settings.newCommentsNav) {
             document.querySelector(".nextC").style.display = "block";
